3ca86ed4cd00726f069c096a40efdf95015488ad
sujiv <sujivraj1204@gmail.com>
2025-11-08 20:38:51 +0530

    Feature: setup and configure k8s using KIND and vite proxy change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path,
         configure: (proxy) => {
